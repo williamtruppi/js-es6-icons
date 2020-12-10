34dbd0e15e267fef1646d04dc4a106f82f62736d
William Truppi <williamt@hotmail.it>
2020-12-10 16:50:40 +0100

    Primo commit

---
+++ milestone1/icons.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
+
+  // Semezioniamo il container icons
+
+  //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+
+});
+
+
+/* ---- FUNCTIONS ----*/
+// Creiamo una funzione per iterare nell'array e che appenda al container le icone.
+// tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
+// tip: il template literal ci puo aiutare con il markup
