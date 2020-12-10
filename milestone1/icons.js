bf9024650646f45289b2c516c17e09f61495e416
William Truppi <williamt@hotmail.it>
2020-12-10 18:19:34 +0100

    milestone1 completata

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -11,18 +11,42 @@ $(document).ready(function () {
   }
 
   const iconsList = [
-    new icone ("cat", "fas", "fa-cat", "animals"),
-    new icone ("crow", "fas", "fa-crow", "animals"),
-    new icone ("dog", "fas", "fa-dog", "animals"),
-    new icone ("dragon", "fas", "fa-dragon", "animals"),
-    new icone ("fish", "fas", "fa-fish", "animals"),
-    new icone ("horse", "fas", "fa-horse", "animals"),
+    new icone("cat", "fas", "fa-cat", "animals"),
+    new icone("crow", "fas", "fa-crow", "animals"),
+    new icone("dog", "fas", "fa-dog", "animals"),
+    new icone("dove", "fas", "fa-dove", "animals"),
+    new icone("dragon", "fas", "fa-dragon", "animals"),
+    new icone("horse", "fas", "fa-horse", "animals"),
+    new icone("hippo", "fas", "fa-hippo", "animals"),
+    new icone("fish", "fas", "fa-fish", "animals"),
+    new icone("carrot", "fas", "fa-carrot", "fruits & vegetables"),
+    new icone("apple-alt", "fas", "fa-apple-alt", "fruits & vegetables"),
+    new icone("lemon", "fas", "fa-lemon", "fruits & vegetables"),
+    new icone("pepper-hot", "fas", "fa-pepper-hot", "fruits & vegetables"),
+    new icone("user-astronaut","fas", "fa-user-astronaut","users & people"),
+    new icone("user-graduate","fas", "fa-user-graduate","users & people"),
+    new icone("user-ninja","fas", "fa-user-ninja","users & people"),
+    new icone("user-secret","fas", "fa-user-secret","users & people"),
   ]
 
   console.log(iconsList);
-  // Semezioniamo il container icons
+
+  // Selezioniamo il container icons
+
+  const icons = document.getElementById("icons");
 
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
+
+  iconsList.forEach(element => {
+    const markup = `
+    <div>
+       <i class="${element.prefix} ${element.type}"></i>
+       <div class="title">${element.name.toUpperCase()}</div>
+    </div>
+    `;
+
+    icons.insertAdjacentHTML("beforeend", markup);
+  });
 
 });
 
