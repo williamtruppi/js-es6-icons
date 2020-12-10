11906525578cbe5f69c8e6ee4e5a47de876e7697
William Truppi <williamt@hotmail.it>
2020-12-10 17:14:07 +0100

    Creato array di oggetti

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
-  // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
+  // Creiamo array di oggetti per le icone con le seguenti proprieta: 
+  // name, prefix, type, family 
+  class icone {
+    constructor(name, prefix, type, family){
+      this.name = name;
+      this.prefix = prefix;
+      this.type = type;
+      this.family = family;
+    }
+  }
 
+  const iconsList = [
+    new icone ("cat", "fas", "fa-cat", "animals"),
+    new icone ("crow", "fas", "fa-crow", "animals"),
+    new icone ("dog", "fas", "fa-dog", "animals"),
+    new icone ("dragon", "fas", "fa-dragon", "animals"),
+    new icone ("fish", "fas", "fa-fish", "animals"),
+    new icone ("horse", "fas", "fa-horse", "animals"),
+  ]
+
+  console.log(iconsList);
   // Semezioniamo il container icons
 
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
